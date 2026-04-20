06fbf5a6c088f79396805862ab3d14e08d0923fc
CreatorSXY <davidsxyofficial@gmail.com>
2026-04-20 19:36:28 +0800

    Delete dna-theme-woocommerce-v12.4 directory

--- dna-theme-woocommerce-v12.4/page.php
+++
@@ -1,3 +0,0 @@
-<?php get_header(); ?>
-<main class="page"><div class="container"><?php dna_render_rank_math_breadcrumbs(); ?><?php if (have_posts()) { while (have_posts()) { the_post(); ?><h1 class="page-title"><?php the_title(); ?></h1><?php the_content(); } } ?></div></main>
-<?php get_footer(); ?>
--- dna-theme-woocommerce-v12.4/woocommerce.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-<main id="primary" class="site-main woocommerce-main">
-  <?php dna_render_rank_math_breadcrumbs(); ?>
-  <?php woocommerce_content(); ?>
-</main>
-<?php get_footer(); ?>
